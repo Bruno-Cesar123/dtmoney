cafe45dee5913206ae4f59a2690d6a353abba22b
Bruno Cesar <brunocesar12386@gmail.com>
2021-09-16 16:11:28 -0300

    style form modal

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,50 @@
+import Modal from 'react-modal';
+
+import closeImg from '../../assets/close.svg';
+
+import { Container } from './styles';
+
+interface NewTransactionModalprops {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalprops) {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button 
+        type="button" 
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar modal" />
+      </button>
+      <Container>
+        <h2>cadastrar transação</h2>
+
+        <input
+          placeholder="Título"
+        />
+
+        <input
+          type="number"
+          placeholder="Valor"
+        />
+
+        <input
+          placeholder="Categoria"
+        />
+
+        <button type="submit">
+          Cadastrar
+        </button>
+      </Container>
+
+    </Modal>
+  )
+}
